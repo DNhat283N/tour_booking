3ff13e863c413411050635bdd7b1684aebb2af5c
DNhat283N <nhat.nd283@gmail.com>
2024-08-30 19:11:21 +0700

    Increase minimun sdk to use Locale in UtilComposes

--- TourBooking/app/build.gradle.kts
+++ TourBooking/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.project17.tourbooking"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.places)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
